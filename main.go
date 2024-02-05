143f8f1d3e8d7d1f0259ff01c88062c72bb80e5b
Shaurya Jha <shauryajha1602@gmail.com>
2024-02-05 20:44:01 +0530

    setting up server functions

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// formhandler function
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+
+	fmt.Fprintf(w, "POST request successful")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name = %s \n", name)
+	fmt.Fprintf(w, "Address = %s \n", address)
+}
+
+// hellohandler function
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func main() {
+	// initialize a web server with the http protocol
+	fileServer := http.FileServer(http.Dir("./static"))
+
+	// this will serve the index.html file
+	http.Handle("/", fileServer)
+
+	// show us the form.html
+	http.HandleFunc("/form", formHandler)
+
+	// show the print hello on the webpage
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.Printf("Starting server at port : 8080 \n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
